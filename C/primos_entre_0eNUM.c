3e9fc046ffbc183e432b473a4dc54e57f87bb4d8
Diogo Carvalho Viegas <sasbrazilian@gmail.com>
2024-08-01 20:07:50 -0300

    Add files via upload

---
+++ C/primos_entre_0eNUM.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Implemente uma função para achar primo
+
+void primo(int num){
+  int i, j, primo;
+  for(i = 2; i <= num; i++){
+    primo = 1;
+    for(j = 2; j <= i/2; j++){
+      if(i % j == 0){
+        primo = 0;
+        break;
+      }
+    }
+    if(primo == 1){
+      printf("%d ", i);
+    }
+  }
+  printf("\n");
+}
+
+int main(){
+  printf("Insira o numero: ");
+  int num; scanf("%d",&num);
+
+  primo(num);
+}
